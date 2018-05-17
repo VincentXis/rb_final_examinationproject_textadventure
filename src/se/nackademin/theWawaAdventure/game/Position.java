1da077b73fbbccf8f94631413d4c4abd9c59e93a
VincentXis <rickard.bernal@gmail.com>
2018-05-17 07:44:02 +0200

    removed old and unused code from the position object

--- src/se/nackademin/theWawaAdventure/game/Position.java
+++ src/se/nackademin/theWawaAdventure/game/Position.java
@@ -1,10 +1,8 @@
 package se.nackademin.theWawaAdventure.game;
 
-import java.util.List;
 import java.util.Objects;
 
 public class Position {
-    private List<Position> connectedPositions;
     private int y;
     private int x;
 
@@ -27,17 +25,12 @@ public class Position {
         if (o == null || getClass() != o.getClass()) return false;
         Position position = (Position) o;
         return getY() == position.getY() &&
-                getX() == position.getX() &&
-                Objects.equals(connectedPositions, position.connectedPositions);
+                getX() == position.getX();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(connectedPositions, getY(), getX());
+        return Objects.hash(getY(), getX());
     }
 
-    @Override
-    public String toString() {
-        return String.format("pos: %d,%d", getY(), getX());
-    }
 }
